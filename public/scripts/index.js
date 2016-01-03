abeeeed2fc809ff9234d1e4e400273d2195f0ce1
Karolass <tark810145@gmail.com>
2016-01-03 10:54:14 +0800

    scroll bug fixed

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
     $(window).scroll(function() {
     	var loadmore = $("button#loadMore");
+        if (loadmore.length <= 0)   return;
 	    if(loadmore.position().top < $(window).scrollTop() + $(window).height()) {
 	    	loadmore.click();
 	    }
