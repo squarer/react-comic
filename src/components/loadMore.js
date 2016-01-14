bb7386efcba56ae824d58c314f2efb5e5a824993
squarerliao <squarerliao@gmail.com>
2016-01-14 14:59:18 +0800

    move scroll event to loadmore

--- src/components/loadMore.js
+++ src/components/loadMore.js
@@ -1,6 +1,17 @@
 var React = require('react');
 
 var LoadMore = React.createClass({
+  componentDidMount: function() {
+    window.addEventListener('scroll', function() {
+      var loadmore = $("button#loadMore");
+      if (loadmore.length <= 0) {
+        return;
+      }
+      if(loadmore.position().top < $(window).scrollTop() + $(window).height()) {
+        loadmore.click();
+      }
+    }, false);
+  },
   render: function() {
     return (
       <button type="button" id="loadMore" className="btn btn-default" onClick={this.props.loadMore}>
--- src/public/scripts/index.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    $(window).scroll(function() {
-    	var loadmore = $("button#loadMore");
-        if (loadmore.length <= 0)   return;
-	    if(loadmore.position().top < $(window).scrollTop() + $(window).height()) {
-	    	loadmore.click();
-	    }
-	});
-});
