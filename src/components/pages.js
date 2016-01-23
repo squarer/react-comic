77f6cfb7d30c77a85657a319faa99cfe84580c9d
squarerliao <squarerliao@gmail.com>
2016-01-24 02:23:37 +0800

    remove pagination

--- src/components/pages.js
+++ src/components/pages.js
@@ -1,5 +1,4 @@
 var React = require('react');
-var Pagination = require('./pagination');
 var Breadcrumbs = require('./breadcrumbs');
 var Page = require('./page');
 var ReturnTop = require('./returnTop');
@@ -17,14 +16,14 @@ var Pages = React.createClass({
     window.removeEventListener('scroll', this.handleScroll);
   },
   handleScroll: function() {
-    var pagination = $(".pagination");
-    if (pagination.length <= 0) {
+    var endFlag = $(".end-flag");
+    if (endFlag.length <= 0) {
       return;
     }
     if ($.active > 0) {
       return;
     }
-    if(pagination.position().top < $(window).scrollTop() + $(window).height()) {
+    if(endFlag.position().top < $(window).scrollTop() + $(window).height()) {
       this.loadNextPage();
     }
   },
@@ -33,44 +32,7 @@ var Pages = React.createClass({
       return;
     this.setState({loadPageCount: this.state.loadPageCount + 1});
   },
-  toNext: function(e) {
-    var keyCode = e.keyCode || 39;
-    var tag = e.target.tagName.toLowerCase();
-    if (keyCode !== 39 && keyCode !== 75 || this.isInputTag(tag)) {
-      return false;
-    }
-    var currentIndex = this.getCurrentIndex();
-    if (currentIndex === this.props.pages.length) {
-      return false;
-    }
-    window.location.href = this.getUrl(currentIndex+ 1);
-  },
-  toPrevious: function(e) {
-    var keyCode = e.keyCode || 37;
-    var tag = e.target.tagName.toLowerCase();
-    if (keyCode !== 37 && keyCode !== 74 || this.isInputTag(tag)) {
-      return false;
-    }
-    var currentIndex = this.getCurrentIndex();
-    if (currentIndex === 1) {
-      return false;
-    }
-    window.location.href = this.getUrl(currentIndex - 1);
-  },
-  isInputTag: function(tag) {
-    return tag === 'input' || tag === 'textarea';
-  },
-  getUrl: function(index) {
-    var href = window.location.href;
-    href = href.substring(0, href.lastIndexOf('/page/')) + '/page/' + index;
-    return href;
-  },
-  getCurrentIndex: function() {
-    return this.props.pageIndex < 1 ? 1 : parseInt(this.props.pageIndex);
-  },
   render: function() {
-    var index = this.getCurrentIndex();
-
     var loadPage = [];
     for (var i = 0; i < this.state.loadPageCount; i++) {
       var url = this.props.pages[i];
@@ -86,7 +48,9 @@ var Pages = React.createClass({
           {pageNodes}
         </div>
         <div className="text-center">
-          <Pagination pages={this.props.pages} currentIndex={index} getUrl={this.getUrl} />
+          <div className="alert alert-info end-flag" role="alert">
+            bottom of content
+          </div>
         </div>
         <ReturnTop />
       </div>
