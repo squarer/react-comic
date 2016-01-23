f999cb61d1715a5966c7681178785acf14d61a5b
squarerliao <squarerliao@gmail.com>
2016-01-24 02:16:03 +0800

    add page style

--- src/components/page.js
+++ src/components/page.js
@@ -3,7 +3,9 @@ var React = require('react');
 var Page = React.createClass({
   render: function() {
     return (
-      <img className="page-img" src={this.props.url} />
+      <div className="well">
+        <img className="page-img" src={this.props.url} />
+      </div>
     );
   }
 });
