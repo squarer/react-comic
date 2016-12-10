4a71559e449baec46cb50f21c16cc7962f365eee
squarer <squarerliao@gmail.com>
2016-12-11 06:07:13 +0800

    fix test

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(__dirname + '/src/dist'));
-app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
-});
+if(!module.parent){
+  app.listen(app.get('port'), function() {
+    console.log('Server started: http://localhost:' + app.get('port') + '/');
+  });
+}
 
 module.exports = app;
